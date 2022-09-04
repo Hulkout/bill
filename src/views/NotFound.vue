a4e71cafcd19ab5d8ee67c3cda195736823049e1
jackma <1427113689@qq.com>
2022-09-04 12:18:01 +0800

    添加404

---
+++ src/views/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+当前页面不存在 请检查网址是否正确。
+  <a href="#/">返回首页</a>
+  <router-link to="/">返回首页</router-link>
+</div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'NotFound'
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
