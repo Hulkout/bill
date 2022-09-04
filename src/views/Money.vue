ff7a89e6e5b7343d280cc2ce911ddb287bb09261
jackma <1427113689@qq.com>
2022-09-04 12:38:49 +0800

    Money页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,43 @@
 <template>
-<div>
-  Money
+<div class="nav-wrapper">
+  <div class="content">
+
+    <p>Money</p>
+    <p>Money</p>
+    <p>Money</p><p>Money</p><p>Money</p>
+    <p>Money</p>
+    <p>Money</p>
+    <p>Money</p>
+    <p>Money</p>
+    <p>Money</p>
+    <p>Money</p>
+    <p>Money</p><p>Money</p><p>Money</p>
+    <p>Money</p>
+    <p>Money</p>
+    <p>Money</p>
+    <p>Money</p>    <p>Money</p>
+    <p>Money</p>
+    <p>Money</p><p>Money</p><p>Money</p>
+    <p>Money</p>
+    <p>Money</p>
+    <p>Money</p>
+    <p>Money</p>    <p>Money</p>
+    <p>Money</p>
+    <p>Money</p><p>Money</p><p>Money</p>
+    <p>Money</p>
+    <p>Money</p>
+    <p>Money</p>
+    <p>Money</p>    <p>Money</p>
+    <p>Money</p>
+    <p>Money</p><p>Money</p><p>Money</p>
+    <p>Money</p>
+    <p>Money</p>
+    <p>Money</p>
+    <p>Money</p>
+
+
+  </div>
+
   <Nav/>
 </div>
 </template>
@@ -13,5 +50,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+  .nav-wrapper{
+    border: 1px solid blue;
+    display: flex;
+    flex-direction: column;
+    height: 100vh; //确定 容器的高度
+  }
+  .content{
+    border: 1px solid green;
+    flex-grow: 1;//让内容有多少占多少
+    overflow: auto; //当内容太多了出滚动条 而不是把我的nav顶出屏幕
+  }
 </style>
