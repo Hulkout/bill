650a9a342c80880c12d6c2808144059c9c07f472
jackma <1427113689@qq.com>
2022-09-11 10:43:23 +0800

    完成types Ts

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -7,23 +7,43 @@
   </div>
 </template>
 
-<script lang="js">
-export default {
-  name: 'Types',
-  data(){
-    return{
-      type:'-',// - 表示支出 +表示收入
-    }
-  },
-  methods:{
-    selectType(type){ //接收一个type(+或-) 让data中的type等于这个type  如果不是+ - 报错
-      if (type !== '-'&& type !== '+'){
-        throw new Error('type is unknown')
-      }
-      this.type = type
-    }
+<script lang="ts">
+// export default {
+//   name: 'Types',
+//   data(){
+//     return{
+//       type:'-',// - 表示支出 +表示收入
+//     }
+//   },
+//   methods:{
+//     selectType(type){ //接收一个type(+或-) 让data中的type等于这个type  如果不是+ - 报错
+//       if (type !== '-'&& type !== '+'){
+//         throw new Error('type is unknown')
+//       }
+//       this.type = type
+//     }
+//   }
+// };
+import Vue from 'vue'
+
+import {Component} from 'vue-property-decorator';
+//上面这个库是啥呢'vue-property-decorator' ，它不是由于西写的，因为它写的比由于西的好，由于西写的叫vue-class-component(官方提供的TS支持库)
+//装饰器 Component 告诉 script 下面那个是Vue的组件，这些东西会自动被处理成data methods，不用深究
+@Component
+//还有好多装饰器 去网上看vue-property-decorator这个库的用法 component这个修饰器也是它用的官方的
+
+export default class Types extends Vue{
+  //data 写法，在这里写的任何赋值语句都会变成这个实例的属性，实例的属性自动变为data
+  type = '-'// - 表示支出 +表示收入
+  //methods  这里type必须说明类型只有这是TS加的
+  selectType(type:string){ //接收一个type(+或-) 让data中的type等于这个type  如果不是+ - 报错
+   if (type !== '-'&& type !== '+'){
+    throw new Error('type is unknown')
+   }
+    this.type = type
   }
-};
+
+}
 </script>
 
 <style lang="scss" scoped>
