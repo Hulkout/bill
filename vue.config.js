343878c875f13440a2900a3a50ba674b20379796
jackma <1427113689@qq.com>
2022-09-24 16:21:52 +0800

    提示手机浏览

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,9 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path')
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/bill-website/'
+        : '/',
   lintOnSave: false,
     chainWebpack: config =>{
         const dir = path.resolve(__dirname, 'src/assets/icons')
